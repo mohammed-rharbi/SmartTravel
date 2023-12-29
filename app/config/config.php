0a5aaf78ad3dcfe892570c7857e5e633928bdc0e
Wail Boukhari <wail.boukhari002@gmail.com>
2023-12-29 10:32:37 +0100

    SmartTravel

---
+++ app/config/config.php
@@ -0,0 +1,13 @@
+<?php
+// DB Params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '123');
+define('DB_NAME', 'SamrtTravelv2');
+
+// App Root
+define('APPROOT', dirname(dirname(__FILE__)));
+// URL Root
+define('URLROOT', 'http://localhost/SmartTravel.v2');
+// Site Name
+define('SITENAME', 'SamrtTravel');
